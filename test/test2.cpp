54d2e6d5ff43aa83667a2ad16f9e915d1f05e8e0
PichuginSergey <PichuginSM@ya.ru>
2020-03-14 14:58:29 +0300

    added tests

---
+++ test/test2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include "../src/fastRemoving.h"
+#include "../src/utils.h"
+
+int main() 
+{
+	std::vector<int> vec {2,2,4,5,6,2,2,7,8,2,2};
+	std::cout << "Init vector:\n";
+	print(vec);
+	auto new_last = fast_remove_if(std::begin(vec), std::end(vec), 
+		[](const int elm) { return !(elm % 2); });
+	vec.erase(new_last, std::end(vec));
+	std::cout << "After fast_remove:\n";
+	print(vec);
+	
+	return EXIT_SUCCESS;
+}
